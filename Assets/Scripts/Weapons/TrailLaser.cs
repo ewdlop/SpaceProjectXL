dc21e4fd1cc2f27baebe9b02beb49a0ba48e7021
ewdlop <ray810815@gmail.com>
2020-12-03 15:21:38 -0500

    Modifiy TrailLaser mechaincs

--- Assets/Scripts/Weapons/TrailLaser.cs
+++ Assets/Scripts/Weapons/TrailLaser.cs
@@ -1,60 +1,65 @@
 ﻿using UnityEngine;
 
-public class TrailLaser : Weapon {
+public class TrailLaser : Weapon
+{
 
     private bool isFiredFromRight;
     private GameObject target;
     private Enemy[] enemies;
-    void Start()
-    {
+    void Start() {
         float launchAngletoRad;
+        float random = Random.Range(-90, 0) * Mathf.Deg2Rad;
         if (isFiredFromRight)
-            launchAngletoRad = Random.Range(-90, 0) * Mathf.Deg2Rad;
+            launchAngletoRad = random;
         else
-            launchAngletoRad = Random.Range(-180, -90) * Mathf.Deg2Rad;
-        Vector2 relativeVelocity =
-            speed * new Vector2(Mathf.Cos(launchAngletoRad),
+            launchAngletoRad = Mathf.PI - random;
+        gameObject.GetComponent<Rigidbody2D>().velocity = speed * new Vector2(Mathf.Cos(launchAngletoRad),
             Mathf.Sin(launchAngletoRad));
-        gameObject.GetComponent<Rigidbody2D>().velocity = relativeVelocity;
     }
 
-    void Update()
-    {
+    void Update() {
         Kinematics();
     }
 
-    public override void Shoot(Transform ship, Transform leftFire, Transform rightFire)
-    {
+    public override void Shoot(Transform ship, Transform leftFire, Transform rightFire) {
         Instantiate(gameObject, leftFire.position,
                leftFire.rotation);
-        GameObject rightProjectile;
-        rightProjectile = Instantiate(gameObject, rightFire.position,
-                 rightFire.rotation);
+        GameObject rightProjectile = Instantiate(gameObject,
+                                      rightFire.position,
+                                      rightFire.rotation);
         rightProjectile.GetComponent<TrailLaser>().isFiredFromRight = true;
     }
 
-    public override void Kinematics()
-    {
-        if (target == null)
-        {
+    public override void Kinematics() {
+        if (target == null) {
             enemies = FindObjectsOfType<Enemy>();
-            if (enemies.Length > 0)
-            {
+            if (enemies.Length > 0) {
                 int random = Random.Range(0, enemies.Length);
                 target = enemies[random].gameObject;
             }
-            else
-            {
-
+        } else {
+            float velocityRadian = Mathf.Atan2(gameObject.GetComponent<Rigidbody2D>().velocity.y, gameObject.GetComponent<Rigidbody2D>().velocity.x);
+            Vector3 targetDisplacement = target.transform.position - gameObject.transform.position;
+            float currentSpeed = gameObject.GetComponent<Rigidbody2D>().velocity.magnitude;
+            float cosine = Vector3.Dot(targetDisplacement, gameObject.GetComponent<Rigidbody2D>().velocity) / (targetDisplacement.magnitude * currentSpeed);
+            float displacmentArcRad = Mathf.Acos(cosine);//[0,Pi]
+            float maxTurnRad = Mathf.Min(displacmentArcRad, Mathf.PI * 0.95f);
+            if (displacmentArcRad < 0.1f) {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(speed * 3f * new Vector2(Mathf.Cos(velocityRadian), Mathf.Sin(velocityRadian)), ForceMode2D.Impulse);
+                return;
             }
-        }
-        else
-        {
-            float deltaX = target.transform.position.x - gameObject.transform.position.x;
-            float deltaY = target.transform.position.y - gameObject.transform.position.y;
-            float angleRad = Mathf.Atan2(deltaY, deltaX);
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.5f* Mathf.Cos(angleRad), 0.5f * Mathf.Sin(angleRad)),ForceMode2D.Impulse);
-          
+
+            if (isFiredFromRight) {
+                    Vector2 deltaVelocity = new Vector2(
+    speed * (Mathf.Cos(velocityRadian + 5f * maxTurnRad * Time.deltaTime) - Mathf.Cos(velocityRadian)),
+    speed * (Mathf.Sin(velocityRadian + 5f * maxTurnRad * Time.deltaTime) - Mathf.Sin(velocityRadian)));
+                    gameObject.GetComponent<Rigidbody2D>().AddForce(deltaVelocity, ForceMode2D.Impulse);
+            } else {
+                Vector2 deltaVelocity = new Vector2(
+                   speed * (Mathf.Cos(velocityRadian - 5f * maxTurnRad * Time.deltaTime) - Mathf.Cos(velocityRadian)),
+                   speed * (Mathf.Sin(velocityRadian - 5f * maxTurnRad * Time.deltaTime) - Mathf.Sin(velocityRadian)));
+                gameObject.GetComponent<Rigidbody2D>().AddForce(deltaVelocity, ForceMode2D.Impulse);
+            }
         }
     }
 }
